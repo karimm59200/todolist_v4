34f9b7ed4027f418ab0eadd35741a6f25432208c
karimm59200 <karimm991@gmail.com>
2023-05-19 17:46:18 +0200

    delete main

--- Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.example.controller.ToDoListAppConsole;
-
-public class Main {
-    public static void main(String[] args) {
-        ToDoListAppConsole.main();
-    }
-}
